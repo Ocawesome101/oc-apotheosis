41236a24d593552a03d3b8d4e528392d041e05b5
Ocawesome101 <ocawesome101@gmail.com>
2020-12-28 23:42:47 -0500

    updates (epitome)

--- build/etc/rc.d/0/00_shutdown.lua
+++ build/etc/rc.d/0/00_shutdown.lua
@@ -1,12 +1,3 @@
--- shut down
+-- stub: shut down
 
-local process = require("process")
-local out = io.stdout
-
-out:write("init: sending SIGTERM to all processes\n")
-
-for _, pid in pairs(process.list()) do
-  process.signal(pid, process.signals.SIGTERM)
-end
-
-out:write("init: requesting kernel shutdown")
+print("init: requesting kernel shutdown")
